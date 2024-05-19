8298d68b064a071a5677cd5e2c553075eaaa2dc0
shahdivya5 <shahdivya5sd@gmail.com>
2024-05-19 07:36:39 +0530

    added read input from users

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -19,6 +19,18 @@ namespace Calculator
                 |Multiplication  |*      | a * b = result   |
                 |Division        |/      | a / b = result   |
                 |----------------|-------|------------------|");
+
+            Console.Write("\nEnter First Number:");
+            double num1 = 0;
+            num1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("\nEnter Second Number:");
+            double num2 = 0;
+            num2 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("\nEnter Operation Symbol:");
+            string symb = Console.ReadLine() ?? "+";
+            if (symb == "X" ^ symb == "x") { symb = "*"; }
         }
         public static void Main(string[] args)
         {
