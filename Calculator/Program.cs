73bcb31cc87cb426440e31989101ec86ac644f6d
mayurssoni <mayurssoni@hotmail.com>
2024-05-19 07:23:03 +0530

    Basic program outline created

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Calculator
+{
+    public static class Program
+    {
+        public static void Calculation()
+        {
+
+            Console.WriteLine(@"
+                Operations Cheat Sheet:
+                
+                |----------------|-------|------------------|
+                |Operation       |Symbol |Calculation       |
+                |----------------|-------|------------------|
+                |Addition        |+      | a + b = result   |
+                |Subtraction     |-      | a - b = result   |
+                |Multiplication  |*      | a * b = result   |
+                |Division        |/      | a / b = result   |
+                |----------------|-------|------------------|");
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(@"Basic Calculator Program");
+            Program.Calculation();
+        }
+    }
+}
